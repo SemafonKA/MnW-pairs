5d58f24b4b947976aaf4d96305a1f8f5fe2abf8d
SemafonKA <s_se@ngs.ru>
2020-03-12 16:24:28 +0700

    First start

---
+++ MnW-pairs/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+constexpr auto countPeople = 4;
+const string fManPriority = "\\Priorities\\manPriority.txt";
+const string fWomanPriority = "\\priorities\\womanPriority.txt";
+
+void fillPriorityFile(const string adress) {
+	fstream file(adress, ios_base::in | ios_base::out | ios_base::trunc);
+	if (!file) throw logic_error("Îרטבךא ס פאיכמל");
+	file << 1 << 2 << 3 << 4;
+	file.close();
+}
+
+int main() {
+	fillPriorityFile(fManPriority);
+	return 0;
+}
