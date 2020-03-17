741910c97e10b453e394727b76295c8f9726076e
Semyon <s_se@ngs.ru>
2020-03-17 22:54:39 +0700

    Need for testing

--- MnW-pairs/main.cpp
+++ MnW-pairs/main.cpp
@@ -6,17 +6,24 @@
 #include <string>
 #include <conio.h>
 #include "Dlist.h"
+#include "Chrono_Timer.h"
 using namespace std;
 
-const auto peopleCount = 4;
+const auto peopleCount = 5;
 namespace matrix {
 	using matrix = vector<vector<int>>;
 	void out(matrix& mas) {
+		cout << "\t";
+		for (int i{ static_cast<int>(mas.at(0).size()) }; i > 0; --i) {
+			cout << "\t[" << i << "]";
+		}
+		cout << endl;
+
 		int i{ 0 };
 		for (auto& elem : mas) {
-			cout << i << ") ";
+			cout << i << ")\t\t";
 			for (auto& values : elem) {
-				cout << values << " ";
+				cout << values << "\t";
 			}
 			cout << endl;
 			++i;
@@ -77,7 +84,7 @@ int priorityFileGet(const string& adress, matrix::matrix& mas) {
 		ss.clear();
 		ss << str;
 		static int num{};
-		while (ss >> num && !ss.eof()) {
+		while (ss >> num) {
 			elem.push_back(num);
 		}
 	}
@@ -86,12 +93,40 @@ int priorityFileGet(const string& adress, matrix::matrix& mas) {
 	return size;
 }
 
+void pairMaker(matrix::matrix& man, matrix::matrix woman) {
+	vector<bool> iswFree;
+	vector<bool> ismFree;
+	ismFree.resize(woman.size());
+	iswFree.resize(man.size());
+	for (int i{ 0 }; i < iswFree.size(); ++i) { iswFree.at(i) = true; }
+	for (int i{ 0 }; i < ismFree.size(); ++i) { ismFree.at(i) = true; }
+
+	int wPos{}, summary{};
+	for (int i{ 0 }; i < woman.size(); ++i) {
+		for (int k{ 0 }; k < woman.size(); ++k) {
+			for (int j{ 0 }; j < man.size(); ++j) {
+				if (!ismFree.at(j)) continue;
+				wPos = man.at(j).at(k);
+				if (iswFree.at(wPos) && woman.at(wPos).at(i) == j) {
+					ismFree.at(j) = false;
+					iswFree.at(wPos) = false;
+					cout << "[" << j << "]\t><\t[" << wPos << "]\t Эффектность: (" << woman.size() - k << 
+						"+" << man.size() - i << ")" << endl;
+					summary += woman.size() - k + man.size() - i;
+				}
+			}			
+		}
+	}
+	cout << "\t\t\t Суммарно:    (" << summary << ")" << endl;
+}
+
+
 int main() {
 	system("chcp 65001"); system("cls");
 
 	matrix::matrix man;
 	matrix::matrix woman;
-	
+
 	for (bool isLooped = true; isLooped; ) {
 		cout << "Выберите вариант создания матрицы: " << endl;
 		cout << "  1) Получить случайные матрицы (без записи в файл)" << endl <<
@@ -99,11 +134,15 @@ int main() {
 			"  3) Загрузить матрицы из файлов" << endl;
 		switch (_getch()) {
 		case 49:
+			system("cls");
+			cout << "\nМассивы заполняются...\n";
 			matrix::random(man, peopleCount);
 			matrix::random(woman, peopleCount);
 			isLooped = false;
 			break;
 		case 50:
+			system("cls");
+			cout << "\nМассивы заполняются...\n";
 			matrix::random(man, peopleCount);
 			matrix::random(woman, peopleCount);
 			priorityFileFill(fManPriority, man);
@@ -111,17 +150,28 @@ int main() {
 			isLooped = false;
 			break;
 		case 51:
+			system("cls");
+			cout << "\nМассивы заполняются...\n";
 			priorityFileGet(fManPriority, man);
 			priorityFileGet(fWomanPriority, woman);
 			isLooped = false;
 			break;
 		}
+
 	}
 
-	matrix::out(man);
-	matrix::out(woman);
+	system("cls");
+	cout << "\n[Стартовый массив мужчин]\n";
+	if (man.size() < 50)	matrix::out(man);
+	cout << "\n[Стартовый массив женщин]\n";
+	if (woman.size() < 50)	matrix::out(woman);
+
+	Timer tm;
+	cout << "Предполагаемые пары\n М\t><\t Ж" << endl;
+	pairMaker(man, woman);
+	cout << "Выполнено за " << tm.elapsed() << " секунд(-ы)"<< endl;
 
 	cout << "\nДля выхода нажмите ввод";
-	std::cin.get();
+	cin.get();
 	return 0;
 }
